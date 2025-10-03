c89498a3dae0d197ddad453995fae481ebade0bd
GeorgeZCHR <georchr03@gmail.com>
2025-10-03 12:08:22 +0300

    small correction in importer.cs

--- LDtkImporter/Importer.cs
+++ LDtkImporter/Importer.cs
@@ -12,19 +12,13 @@ public static class Importer
 
         string json = File.ReadAllText(path);
 
-        Console.WriteLine(json.Substring(0, 200));
-
         var options = new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true // optional: LDtk uses camelCase
         };
         options.Converters.Add(new JsonStringEnumConverter());
 
-        LDtkFile f = JsonSerializer.Deserialize<LDtkFile>(json, options)
+        return JsonSerializer.Deserialize<LDtkFile>(json, options)
             ?? throw new InvalidDataException("Failed to deserialize LDtk file.");
-
-        //if (f != null) Console.WriteLine(f.ToString());
-
-        return f;
     }
 }
